0c977703050e3835a4b02f7485098316f7b93b79
Vel <korzhavin.vel@yandex.ru>
2023-08-01 16:29:08 +0700

    zadachki k biletam

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.util.Arrays;
 import java.util.Collections;
-
+import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) {
-task1(); task2(); task6(); task3();
+        task1();
+        task2();
+        task6();
+        task3();
+        task4();
+        task5();
     }
 
     public static void task6() {
@@ -22,10 +27,11 @@ task1(); task2(); task6(); task3();
     }
 
     public static void task2() {
-        String word ="RACEcar";
+        String word = "RACEcar";
         isPalindrome(word);
     }
-    public static String reverseString(String word){
+
+    public static String reverseString(String word) {
         String reversedWord = "";
         for (int i = word.length() - 1; i >= 0; --i)
             reversedWord += word.charAt(i);
@@ -33,16 +39,16 @@ task1(); task2(); task6(); task3();
 
 
     }
+
     public static Boolean isPalindrome(String word) {
         word = word.toLowerCase();
-        if(word.equals(reverseString(word))){
+        if (word.equals(reverseString(word))) {
             System.out.println("Полиндром");
-        }else{
+        } else {
             System.out.println("Не полиндром");
         }
         return word.equals(reverseString(word));
     }
-
 
 
     public static void task3() {
@@ -54,5 +60,33 @@ task1(); task2(); task6(); task3();
         System.out.println(firstHalf);
         System.out.println(secondHalf);
     }
+
+    public static void task4() {
+        String[] words = {"aa", "bb", "cc", "aa", "bb", "dd"};
+        System.out.println(keepDistinct(words));
+    }
+
+    public static String keepDistinct(String... arr) {
+        return Arrays.stream(arr)
+                .distinct()
+                .collect(Collectors.joining(", ", "[", "]")); // в виде "[a, b, c]"
+//      .collect(Collectors.joining(", ")); // с разделителем ", " - в виде "a, b"
+//      .collect(Collectors.joining()); // без разделителя, в виде "ab"
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        int[] firstArray = new int[10];
+        for (int i = 0; i < firstArray.length; i++) {
+            firstArray[i] = i + 1;
+            if (firstArray[i] % 2 != 0) {
+                firstArray[i]++;
+            }
+        }
+        System.out.println(Arrays.toString(firstArray));
+        int[] newArray = Arrays.copyOfRange(firstArray, 3, 7);
+        System.out.println(Arrays.toString(newArray));
+    }
+
 
 }
